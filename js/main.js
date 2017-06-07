a5ec2f979679a1321e1b88948093d77864102c2d
Jan Drenkhahn <jan-dennisd@outlook.at>
2017-06-07 22:15:45 +0200

    navbar beginning and positon of headline and slideshow

--- js/main.js
+++ js/main.js
@@ -1,14 +1,41 @@
-(var myIndex = 0;
-carousel();
-
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("freesliedes");
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";  
+(function() {
+   
+    /*
+        module variables 
+    */
+    var myIndex = 0;
+    var carouseDelay = 3000; // Change image every 3 seconds
+    var carouselImages; // Variable zum speichern des Arrays, in dem alle Bilder abgelegt werden
+    
+    init();  // die gesammelten functionen von unten werden gestartet.
+    
+    /*
+        functions 
+    */
+    function carousel() {
+        for (var i = 0; i < carouselImages.length; i++) {
+           carouselImages[i].style.display = "none";  
+        }
+        myIndex++;
+        if (myIndex > carouselImages.length) {
+            myIndex = 1;
+        }
+        carouselImages[myIndex-1].style.display = "block";
+        setTimeout(carousel, carouseDelay);
+    };
+    
+    function getCarouselImages() {
+        carouselImages = document.getElementsByClassName("freesliedes"); // zum einmaligen laden aller Bilder aus der HTML Datei
     }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}    
-    x[myIndex-1].style.display = "block";  
-    setTimeout(carousel, 3000); // Change image every 2 seconds
+    
+    function setupEvents() {    // für weiter Funktionen
+        
+    }
+    
+    function init() {   // hier werden alle funktionen gesammelt und gesammelt gestartet
+        getCarouselImages();
+        carousel();
+        setupEvents();
+    }
+    
 })();
