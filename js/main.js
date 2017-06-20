b719c114dd1c64ad0a963829735a442395172e7c
Jan Drenkhahn <jan-dennisd@outlook.at>
2017-06-20 22:05:31 +0200

    optimise slideshow and down button

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@
     */
     var myIndex = 0;
     var carouseDelay = 4500; // Change image every 3 seconds
+    var fadeTiming = 2000; // Fade Timing for FadeIn and fadeOut of the images
     var carouselImages; // Variable zum speichern des Arrays, in dem alle Bilder abgelegt werden
     
     init();  // die gesammelten functionen von unten werden gestartet.
@@ -14,13 +15,15 @@
     */
     function carousel() {
         for (var i = 0; i < carouselImages.length; i++) {
-           carouselImages[i].style.display = "none";  
+           $(carouselImages[i]).fadeOut(fadeTiming);
+//         carouselImages[i].style.display = "none";
         }
         myIndex++;
         if (myIndex > carouselImages.length) {
             myIndex = 1;
         }
-        carouselImages[myIndex-1].style.display = "block";
+//        carouselImages[myIndex-1].style.display = "block";myIndex-1
+        $(carouselImages[myIndex-1]).fadeIn(fadeTiming);
         setTimeout(carousel, carouseDelay);
     };
     
