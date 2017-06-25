b6a8d35f0e115c314b6e02ded23e4e493791bae0
Jan Drenkhahn <jan-dennisd@outlook.at>
2017-06-25 14:59:00 +0200

    install change text function

--- js/button.js
+++ js/button.js
@@ -1,6 +1,19 @@
-(function(){
-    function changetogerman() {
-            document.getElementById('welcome').innerHTML = 'hier steht ein anderer text nur für dich';
+//Function to change the text of the page
+
+function changetoaustrian() {
+    document.getElementById('welcome').innerHTML = 'Griass Eich<br><br>Welcome<br>Lust auf Tanzen zu sonnigen Sommerklängen?<br>Dann komm auf eines unserer Konzerte<br>der international zusammengewürfelten<br>in Wien lebenden Roots/Rocksteady/Ska-Band<br>FREEMEYEND.<br>Führe deine Tanzschuhe wieder aus – sie werden es dir danken.<br>Aalright Hawara und Hawararinnen!<br>Anadda leiwande Reggaeband<br>has a serious Kölch with Babylon!<br>11 zuagroaste, glücklich freilaufende Freedom Fighters<br>wanna see you daunz,<br>so swing your Oarsch<br>onto the dancefloor!!!><br>1 ❤ FREEMEYEND<br>Hau weg di BADDA-ration<br>Roots/Rocksteady/Ska outta Vienna.<br><a href="http://freemeyend.at/">www.freemeyend.at</a>'
+}
+
+
+function changetogerman() {
+            document.getElementById('welcome').innerHTML = 'Hier sollte ein text auf hochdeutsch stehen';
         }
+
+function changetoenglish() {
+    document.getElementById('welcome').innerHTML = 'Welcome<br>Wanna dance and move your feet to sunny summery sounds?<br>Then join us at one of our gigs. Wipe the dust off your dancing shoes, take ‘em out for the night and rock the night away with us.<br>We are FREEMEYEND, a Vienna based Reggae/Rocksteady/Ska band whose members originate from all Europe.<br>Let the good vibes roll, let the sound take control, ease up all the stress and let us please your soul.<br><a href="http://freemeyend.at/">www.freemeyend.at</a>'
+}
+
+function changetoitaly() {
+    document.getElementById('welcome').innerHTML ='Ok ragazze e ragazzi!<br>Una reggae-band di Vienna ha un problema serio con Babylon!<br>11 musicisti liberi wanna see you dance,<br>porta il tuo culo sul dancefloor e balla con noi!<br>1 ❤ Freemeyend<br>Butta via la Baddaration!Chi siamo?<br>Siamo 11 musicisti con la mente aperta:<br>dall’ Austria, dalla Germania, dalla Spagna,<br>dalla Slovacchia, dall’ Ungheria<br>e dall’ Italia<br>Suoniamo Roots Rocksteady e Ska!<br>'
+}
  
- })();
