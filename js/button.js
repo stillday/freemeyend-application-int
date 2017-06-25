37db64a94173b04e3eab2636f554200db59dd517
Jan Drenkhahn <jan-dennisd@outlook.at>
2017-06-25 15:16:04 +0200

    change the text formatting

--- js/button.js
+++ js/button.js
@@ -1,7 +1,7 @@
 //Function to change the text of the page
 
 function changetoaustrian() {
-    document.getElementById('welcome').innerHTML = 'Griass Eich<br><br>Welcome<br>Lust auf Tanzen zu sonnigen Sommerklängen?<br>Dann komm auf eines unserer Konzerte<br>der international zusammengewürfelten<br>in Wien lebenden Roots/Rocksteady/Ska-Band<br>FREEMEYEND.<br>Führe deine Tanzschuhe wieder aus – sie werden es dir danken.<br>Aalright Hawara und Hawararinnen!<br>Anadda leiwande Reggaeband<br>has a serious Kölch with Babylon!<br>11 zuagroaste, glücklich freilaufende Freedom Fighters<br>wanna see you daunz,<br>so swing your Oarsch<br>onto the dancefloor!!!><br>1 ❤ FREEMEYEND<br>Hau weg di BADDA-ration<br>Roots/Rocksteady/Ska outta Vienna.<br><a href="http://freemeyend.at/">www.freemeyend.at</a>'
+    document.getElementById('welcome').innerHTML = 'Griass Eich<br><br>Welcome<br>Lust auf Tanzen zu sonnigen Sommerklängen?<br>Dann komm auf eines unserer Konzerte<br>der international zusammengewürfelten<br>in Wien lebenden Roots/Rocksteady/Ska-Band<br>FREEMEYEND.<br>Führe deine Tanzschuhe wieder aus – sie werden es dir danken.<br>Aalright Hawara und Hawararinnen!<br>Anadda leiwande Reggaeband<br>has a serious Kölch with Babylon!<br>11 zuagroaste, glücklich freilaufende Freedom Fighters<br>wanna see you daunz,<br>so swing your Oarsch<br>onto the dancefloor!!!><br>1 ❤ FREEMEYEND<br>Hau weg di BADDA-ration<br>Roots/Rocksteady/Ska outta Vienna.<br><br><a href="http://freemeyend.at/">www.freemeyend.at</a>'
 }
 
 
@@ -10,10 +10,10 @@ function changetogerman() {
         }
 
 function changetoenglish() {
-    document.getElementById('welcome').innerHTML = 'Welcome<br>Wanna dance and move your feet to sunny summery sounds?<br>Then join us at one of our gigs. Wipe the dust off your dancing shoes, take ‘em out for the night and rock the night away with us.<br>We are FREEMEYEND, a Vienna based Reggae/Rocksteady/Ska band whose members originate from all Europe.<br>Let the good vibes roll, let the sound take control, ease up all the stress and let us please your soul.<br><a href="http://freemeyend.at/">www.freemeyend.at</a>'
+    document.getElementById('welcome').innerHTML = 'Welcome<br><br>Wanna dance and move your<br>feet to sunny summery sounds?<br>Then join us at one of our gigs.<br>Wipe the dust off your dancing shoes,<br>take ‘em out for the night<br>and rock the night away with us.<br>We are FREEMEYEND,<br>a Vienna based Reggae/Rocksteady/Ska band<br> whose members originate from all Europe.<br>Let the good vibes roll,<br> let the sound take control,<br> ease up all the stress and let us please your soul.<br><br><a href="http://freemeyend.at/">www.freemeyend.at</a>'
 }
 
 function changetoitaly() {
-    document.getElementById('welcome').innerHTML = 'Ok ragazze e ragazzi!<br>Una reggae-band di Vienna ha un problema serio con Babylon!<br>11 musicisti liberi wanna see you dance,<br>porta il tuo culo sul dancefloor e balla con noi!<br>1 ❤ Freemeyend<br>Butta via la Baddaration!Chi siamo?<br>Siamo 11 musicisti con la mente aperta:<br>dall’ Austria, dalla Germania, dalla Spagna,<br>dalla Slovacchia, dall’ Ungheria<br>e dall’ Italia<br>Suoniamo Roots Rocksteady e Ska!<br><a href="http://freemeyend.at/">www.freemeyend.at</a>'
+    document.getElementById('welcome').innerHTML = 'Ok ragazze e ragazzi!<br><br>Una reggae-band<br>di Vienna ha un problema serio con Babylon!<br>11 musicisti<br>liberi wanna see you dance,<br>porta il tuo culo sul dancefloor e balla con noi!<br>1 ❤ Freemeyend<br>Butta via la Baddaration!Chi siamo?<br>Siamo 11 musicisti con la mente aperta:<br>dall’ Austria, dalla Germania, dalla Spagna,<br>dalla Slovacchia, dall’ Ungheria<br>e dall’ Italia<br>Suoniamo Roots Rocksteady e Ska!<br><br><a href="http://freemeyend.at/">www.freemeyend.at</a>'
 }
  
